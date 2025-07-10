03f63511730e8edeb9407bd7c8a029ae79b2b04a
TheRealMattHUN <legolegend007@gmail.com>
2025-07-10 22:25:16 +0200

    remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/noon-website/',
   plugins: [tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
