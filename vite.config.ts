93d9ca1904e6d0c69e63831bb2ddb3e0ae3603ac
TheRealMattHUN <legolegend007@gmail.com>
2025-07-10 16:47:08 +0200

    hmmmmm

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/noon-website/',
   plugins: [tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
